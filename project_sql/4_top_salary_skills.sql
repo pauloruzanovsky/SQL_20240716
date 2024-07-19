caf4bacc2b296a81f7b5ff97a1a2605493671c01
Paulo Ruzanovsky <pauloruzanovsky@gmail.com>
2024-07-19 12:13:19 -0300

    upload sql files

---
+++ project_sql/4_top_salary_skills.sql
@@ -0,0 +1,27 @@
+/*
+Question: What ar the top skills based on salary?
+- Look at the average salary associated with each skill for Data Analyst positions
+- Focuses on roles with specified salaries, regardless of location
+Why? It reveals how different skills impact salary levels for Data Analysts and helps indentify the 
+    most financially rewarding skills to acquire or improve
+*/
+
+
+SELECT 
+    skills
+    , ROUND(AVG(salary_year_avg/1000),0) AS avg_salary
+FROM 
+    job_postings_fact
+INNER JOIN 
+    skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN 
+    skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst' 
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = true
+GROUP BY skills
+ORDER BY avg_salary DESC
+LIMIT 25
+
+
